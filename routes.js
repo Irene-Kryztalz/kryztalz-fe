718c630e5b17a1b0dbb4de3216fbe69616535f2a
Irene <okaforirenen@gmail.com>
2020-09-07 15:16:46 +0100

    navbar start

---
+++ routes.js
@@ -0,0 +1,35 @@
+export const normal =
+    [
+        {
+            path: "/products",
+            text: "products"
+        },
+        {
+            path: "/",
+            text: "home"
+        }
+    ];
+
+export const auth =
+    [
+        {
+            path: "/sign-in",
+            text: "Sign In"
+        },
+        {
+            path: "/sign-up",
+            text: "sign up"
+        }
+    ];
+
+export const user =
+    [
+        {
+            path: "/order",
+            text: "order history"
+        },
+        {
+            path: "/account",
+            text: "account details"
+        }
+    ];
